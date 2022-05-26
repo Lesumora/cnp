f4d8090775a6e938e760f69bb6c5f2774a99337f
Lesumora <lessermature@gmail.com>
2022-05-26 16:50:24 +0800

    lambda

--- app/src/main/java/com/mawdcnp/planner/lottie/SplashScreen.java
+++ app/src/main/java/com/mawdcnp/planner/lottie/SplashScreen.java
@@ -16,12 +16,10 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashScreen.this, MainActivity.class));
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            finish();
         },2700);
+
     }
 }
