2ed375d36d8f030122100fa7a9245e5f511d2039
Lesumora <lessermature@gmail.com>
2022-05-26 15:01:51 +0800

    Initial commit

---
+++ app/src/main/java/com/mawdcnp/planner/lottie/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.mawdcnp.planner.lottie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.mawdcnp.planner.MainActivity;
+import com.mawdcnp.planner.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        },2700);
+    }
+}
